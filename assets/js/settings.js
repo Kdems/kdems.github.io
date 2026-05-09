d826d853040a69db6a9db210f12b9873765199fc
Kdems <texas_boiler@hotmail.com>
2026-05-09 22:50:47 +0800

    Update settings.js

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,1 +1,78 @@
+const SETTINGS_KEY =
+  "skybar_settings";
 
+
+
+
+
+function getSettings() {
+
+  const raw =
+    localStorage.getItem(
+      SETTINGS_KEY
+    );
+
+
+  if (!raw) {
+
+    return getDefaultSettings();
+
+  }
+
+
+  return JSON.parse(
+    raw
+  );
+
+}
+
+
+
+
+
+function saveSettings(
+  payload
+) {
+
+  localStorage.setItem(
+
+    SETTINGS_KEY,
+
+    JSON.stringify(
+      payload
+    )
+
+  );
+
+}
+
+
+
+
+
+function getDefaultSettings() {
+
+  return {
+
+    currency:
+      "RM",
+
+
+    monthlyBudget:
+      120000,
+
+
+    annualRevenueTarget:
+      1440000,
+
+
+    lyFoodRevenue:
+      45000,
+
+
+    lyBeverageRevenue:
+      30000
+
+  };
+
+}
